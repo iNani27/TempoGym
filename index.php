ecfbe25cc8cd750e449fec13939cb9b3fd5fe19b
Stagiaire <Stagiaire@192.168.178.253>
2015-05-12 16:25:27 +0200

    j'ai fixer les caractères spéciaux des keywords

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Team: Nadia & Isabelle
         <meta charset="UTF-8">
         <title>Tempogym Jette</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
-        <meta name="keywords" content="Tempogym Jette, Tempogym, club, gym, gymnastique, gymnaste, Jette, Bruxelles, tarif, horaire, sport, sportif, service, moniteur, monitrice, professeure, professeur dâ€™Ã©ducation physique, compÃ©tition, spectacle, enfant, fille, garÃ§on, parents, maternelle, primaire, secondaire, Cocof, Commune de Jette, RÃ©gion Bruxelloise, sol, saut, barres, barres asymÃ©triques, poutre, rondade, flic, salto, arriÃ¨re, avant">
+        <meta name="keywords" content="Tempogym Jette, Tempogym, club, gym, gymnastique, gymnaste, Jette, Bruxelles, tarif, horaire, sport, sportif, service, moniteur, monitrice, professeure, professeur d&acute;&eacute;ducation physique, comp&eacute;tition, spectacle, enfant, fille, gar&ccedil;on, parents, maternelle, primaire, secondaire, Cocof, Commune de Jette, Région Bruxelloise, sol, saut, barres, barres asym&eacute;triques, poutre, rondade, flic, salto, arri&egrave;re, avant">
         <link rel="stylesheet" href="styles.css" />
     </head>
     <body>
