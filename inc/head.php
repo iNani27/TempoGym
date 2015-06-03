7ce880016a80a34638cb247f0fc73bc044861141
Stagiaire <Stagiaire@192.168.178.253>
2015-06-03 11:25:41 +0200

    Version responsive + connexion/session galery head

--- inc/head.php
+++ inc/head.php
@@ -1,4 +1,48 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
+<!--
+Projet client @CF2m 2015
+Club Tempogym Jette
+20150631 HW Version Responsive  @iNani
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tempogym Jette</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="keywords" content="tempogym jette, tempogym, club, gym, gymnastique, gymnaste, jette, Bruxelles, tarif, horaire, sport, sportif, service, moniteur, monitrice, professeure, professeur d&acute;&eacute;ducation physique, comp&eacute;tition, spectacle, enfant, fille, gar&ccedil;on, parents, maternelle, primaire, secondaire, Cocof, Commune de Jette, RÃ©gion Bruxelloise, sol, saut, barres, barres asym&eacute;triques, poutre, rondade, flic, salto, arri&egrave;re, avant">
+		<link rel="stylesheet" href="css/resp.css" />
+    </head>
+       <body>
+        <!-- <h1>Tempogym : club de gymnastique Ã  Jette </h1>-->
+        <header>
+            <figure>
+                <img id="responsive_banner" src="img/banner.jpg" alt="Tempogym jette" title="Tempogym jette">
+				<img id="responsive_logo" src="img/logo.png" alt="Tempogym Jette" title="Tempogym Jette">	
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="club.php">Le club</a></li>
+                    <li><a href="dates.php">Dates</a></li>
+                    <li><a href="culture.php">Culture</a></li>
+                    <li><a href="galerie.php">Galerie</a></li>
+                    <li><a href="contact.php">Documents</a></li>
+                    <li><a href="connect.php">Se connecter</a></li>
+                </ul>
+				<!-- Option menu small device ::: Responsiveness -->
+				<select onchange="top.location.href = this.options[this.selectedIndex].value;">
+					<option class="selected" value="" selected="selected">Aller sur le page</option>
+					<option value="index.php">Accueil</option>
+					<option value="club.php">Le club</option>
+					<option value="dates.php">Dates</option>
+					<option value="galerie.php">Galerie</option>
+					<option value="contact.php">Contact</option>
+					<option value="docs.php">Documents</option>
+					<option value="connect.php">Se connecter</option>
+				</select>
+            </nav>
+        </header>    
 <!--
 Projet client @CF2m 2015
 Club Tempogym Jette
