e49fb96371263bd701d82f5260e3548c35be8c0d
Stagiaire <Stagiaire@192.168.178.253>
2015-06-12 12:22:04 +0200

    split_css

--- inc/style_resp.php
+++ inc/style_resp.php
@@ -1,2 +1,3 @@
+<link rel="stylesheet" href="css/color.css" />
+<link rel="stylesheet" href="css/nav_bouton.css" />
 <link rel="stylesheet" href="css/resp.css" />
-<!--<link rel="stylesheet" href="css/nav.css" />-->
