5d1b02af669b6b51241f4500c22b9c922e662cb3
Stagiaire <Stagiaire@192.168.178.253>
2015-06-29 10:18:34 +0200

    CSS debug 2

--- inc/style_resp.php
+++ inc/style_resp.php
@@ -1,6 +1,4 @@
-
-
-<!--<link rel="stylesheet" href="css/color.css" />
-<link rel="stylesheet" href="css/section.css" />-->
+<link rel="stylesheet" href="css/section.css" />
+<link rel="stylesheet" href="css/color.css" />
 <link rel="stylesheet" href="css/nav_bouton.css" />
 <link rel="stylesheet" href="css/resp.css" />
