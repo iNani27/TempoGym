8c489a29bb9abc2bf3fffb4cb10e0776dd74a7e3
Stagiaire <Stagiaire@192.168.178.253>
2015-06-30 16:15:41 +0200

    ok titre en h2

--- contact.php
+++ contact.php
@@ -28,7 +28,7 @@ include_once 'inc/head.php';
             <p>02/672.22.60</p>
         </article>
         <article>
-            <h3>Nouvelle salle bientôt disponible</h3>
+            <h2>Nouvelle salle bientôt disponible</h2>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.1960734972054!2d4.333365400000001!3d50.8830813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c3cedeb09d89%3A0x70469259b3d1f887!2sAvenue+du+Comt%C3%A9+de+Jette+3%2C+1090+Jette!5e0!3m2!1sfr!2sbe!4v1435672326933" width="1100" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </article>
         <?php
