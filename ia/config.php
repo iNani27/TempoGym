7df7d6c0f247dddefe8311553aa624b8ec359675
Stagiaire <Stagiaire@192.168.178.253>
2015-07-03 13:43:35 +0200

    Delivery config local

--- ia/config.php
+++ ia/config.php
@@ -1,13 +1,8 @@
 <?php
 /* local*/
- /* $serveur = "localhost";
+  $serveur = "localhost";
   $login = "root";
   $password = ""; 
   $db = "tempogym";
-*/
 
-/* online */
-  $serveur = "195.144.11.155";
-  $login = "dessycf2m7";
-  $password = "qqRGpd410XPj"; 
-  $db = "dessycf2m7";
+
